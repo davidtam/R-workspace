190aeade521035f1029012cc8a2cd040988b8522
unknown <David.Tam@lpt1134.bjss.co.uk>
2014-11-13 17:25:16 +0000

    now need to consider count in mean

--- pollutantmean.R
+++ pollutantmean.R
@@ -16,16 +16,20 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
         
+        meanVector <- numeric()
+
+        for (currentDataFile in id){
         
-        currentDataFile <- 23
-        
-        paddedCurrentDataFile <- formatC(currentDataFile, width = 3, format = "d", flag = "0") 
-        
-        currentData <- read.csv(paste(paste(directory , paddedCurrentDataFile , sep = "/"), ".csv", sep = ""))
-        
-        currentData
-        
-        filteredNA <- currentData[!is.na(currentData[pollutant]), TRUE, drop=FALSE]
-        
-        mean(unlist(filteredNA[pollutant], use.names = FALSE))
+		paddedCurrentDataFile <- formatC(currentDataFile, width = 3, format = "d", flag = "0") 
+		
+		currentData <- read.csv(paste(paste(directory , paddedCurrentDataFile , sep = "/"), ".csv", sep = ""))
+		
+		currentData
+		
+		filteredNA <- currentData[!is.na(currentData[pollutant]), TRUE, drop=FALSE]
+		
+		thisMean <- mean(unlist(filteredNA[pollutant], use.names = FALSE))
+		meanVector <- c(meanVector, thisMean)
+	}
+	meanVector
 }
