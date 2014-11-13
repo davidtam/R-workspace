fbff6bf955fcbf34ba2b4b1758cb6bdc2d5a6ff4
unknown <David.Tam@lpt1134.bjss.co.uk>
2014-11-13 17:11:45 +0000

    Working now for one the dir and pollutant, need to sort out multiple files

---
+++ pollutantmean.R
@@ -0,0 +1,31 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+
+
+
+	
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+
+        ## 'pollutant' is a character vector of length 1 indicating
+        ## the name of the pollutant for which we will calculate the
+        ## mean; either "sulfate" or "nitrate".
+
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+
+        ## Return the mean of the pollutant across all monitors list
+        ## in the 'id' vector (ignoring NA values)
+        
+        
+        currentDataFile <- 23
+        
+        paddedCurrentDataFile <- formatC(currentDataFile, width = 3, format = "d", flag = "0") 
+        
+        currentData <- read.csv(paste(paste(directory , paddedCurrentDataFile , sep = "/"), ".csv", sep = ""))
+        
+        currentData
+        
+        filteredNA <- currentData[!is.na(currentData[pollutant]), TRUE, drop=FALSE]
+        
+        mean(unlist(filteredNA[pollutant], use.names = FALSE))
+}
