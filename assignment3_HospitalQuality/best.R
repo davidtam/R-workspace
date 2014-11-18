aa7e8942c5588d73b711ced5061ffcae36c0f538
unknown <David.Tam@lpt1134.bjss.co.uk>
2014-11-18 17:00:04 +0000

    done with best.R now neeed to submit

--- assignment3_HospitalQuality/best.R
+++ assignment3_HospitalQuality/best.R
@@ -1,7 +1,30 @@
 best <- function(state, outcome) {
 	## Read outcome data
+	outcomeRaw <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+	
 	## Check that state and outcome are valid
+	if (!is.element(state, outcomeRaw$State)){
+		stop("invalid state")
+	}
+	
+	outcomeCol <- paste ("Hospital.30.Day.Death..Mortality..Rates.from.",
+		gsub(" ", ".", simpleCap(outcome)), sep = "", collapse = NULL)
+	if (!is.element(outcomeCol, names(outcomeRaw))){
+		stop("invalid outcome")
+	}
+	
 	## Return hospital name in that state with lowest 30-day death
 	## rate
+	outcomeFilteredState <- outcomeRaw[outcomeRaw$State == state, ]
+	outcomeFilteredNA <- outcomeFilteredState[complete.cases(outcomeFilteredState[,outcomeCol]), ]
+	outcomeFilteredNA[, outcomeCol] <- as.numeric(outcomeFilteredNA[, outcomeCol])
+	
+	
+	outcomeFilteredNA[which.min(outcomeFilteredNA[, outcomeCol]),"Hospital.Name"]
+}
 
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+      sep="", collapse=" ")
 }
