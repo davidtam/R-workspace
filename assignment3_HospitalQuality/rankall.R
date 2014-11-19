33c91ebf569bc4ae2b7b553f34ad1d393da915f8
unknown <David.Tam@lpt1134.bjss.co.uk>
2014-11-19 14:48:53 +0000

    done with R-programming course

--- assignment3_HospitalQuality/rankall.R
+++ assignment3_HospitalQuality/rankall.R
@@ -1,7 +1,19 @@
 rankall <- function(outcome, num = "best") {
 	## Read outcome data
+	outcomeRaw <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
 	## Check that state and outcome are valid
 	## For each state, find the hospital of the given rank
 	## Return a data frame with the hospital names and the
 	## (abbreviated) state name
+	
+	uniqueStates <- sort(unique(outcomeRaw$State))
+	result <- data.frame(state=uniqueStates)
+	row.names(result) <- result$state
+	
+	for(aState in result$state){
+		result[aState, "hospital"] <- rankhospital(aState, outcome, num)
+	}
+	result[, c(2,1)]
+
 }
